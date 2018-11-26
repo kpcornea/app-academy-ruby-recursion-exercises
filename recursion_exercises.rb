39964dc3df3b7223452348f465277b5876de1fed
Kevin Cornea <kpcornea@gmail.com>
2018-11-26 15:30:50 -0800

    sum array methods completed

--- recursion_exercises.rb
+++ recursion_exercises.rb
@@ -3,3 +3,24 @@ def range(start, finish)
   return [start] if finish - 1 == start
   range(start, finish - 1) + [finish - 1]
 end
+
+def sum_array_iterative(arr)
+  count = arr.first
+
+  i = 1
+  while i < arr.length
+    num = arr[i]
+    count += num
+
+    i += 1
+  end
+
+  count
+end
+
+def sum_array_recursive(arr)
+  return arr.last if arr.length == 1 || arr.empty?
+  arr.last + sum_array_recursive(arr[0...-1])
+end
+
+# arr = [1, 2, 3, 4]
