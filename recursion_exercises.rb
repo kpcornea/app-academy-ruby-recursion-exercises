51441cf1ce98a4bb460fc33da77f7a6ec1388af9
Kevin Cornea <kpcornea@gmail.com>
2018-11-26 16:12:48 -0800

    exponentiation methods completed

--- recursion_exercises.rb
+++ recursion_exercises.rb
@@ -24,3 +24,32 @@ def sum_array_recursive(arr)
 end
 
 # arr = [1, 2, 3, 4]
+
+def exp1(b, n)
+  return 1 if n == 0
+  p b * exp1(b, n - 1)
+end
+
+# version with stack trace printed
+
+# def exp2(b, n)
+#   # puts "returning 1" if n == 0
+#   # return 1 if n == 0
+#   puts "returning b" if n == 1
+#   return b if n == 1
+#   puts "even" if n.even?
+#   even_ans = exp2(b, n / 2) ** 2 if n.even?
+#   # return exp2(b, n / 2) ** 2 if n.even?
+#   return even_ans if n.even?
+#   puts "odd" if n.odd?
+#   odd_ans = b * (exp2(b, (n - 1) / 2) ** 2) if n.odd?
+#   # b * (exp2(b, (n - 1) / 2) ** 2) if n.odd?
+#   odd_ans if n.odd?
+# end
+
+def exp2(b, n)
+  return 1 if n == 0
+  return b if n == 1
+  return exp2(b, n / 2) ** 2 if n.even?
+  b * (exp2(b, (n - 1) / 2) ** 2) if n.odd?
+end
